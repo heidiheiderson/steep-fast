2541dd63b40300b3038acf2a9443842d4588a3b8
heidiheiderson <55721033+heidiheiderson@users.noreply.github.com>
2021-01-14 13:31:31 -0800

    first update

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
 function setup() {
-	createCanvas(800, 800);
+	createCanvas(600, 600);
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
-  ellipse(mouseX, mouseY, 200, 200);
+   line(0, 325, 110, 325); //flat
+  line(110, 325, 125, 225);
+  line(125, 225, 150, 375);
+  line(150, 375, 175, 150);
+  line(175, 150, 200, 450);
+  line(200, 450, 225, 325);
+  line(225, 325, 275, 325);//flat
+  line(275, 325, 300, 325);
+  line(300, 325, 315, 225);
+  line(315, 225, 335, 375);
 }
+
+
+
+
